1b31226600d47d68246981b5c506e29ca6ddef66
Tharun-de <tharunpilli01@gmail.com>
2025-03-04 09:32:43 -0800

    Update AboutPage.tsx

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our company! We provide top-notch facility management and contracting services.
+      </p>
+    </div>
+  );
+};
+
+export default About;
